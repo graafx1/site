75a8831ed3f0d8b703fd40d6e253b0c3e48420ec
Volodymyr Savva <graafx1@gmail.com>
2016-07-13 13:28:39 +0000

    add new file

---
+++ js/search.js
@@ -0,0 +1,9 @@
+(function() {
+    var cx = '007021497031724502665:hihxvp3yacq';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
